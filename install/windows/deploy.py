200b1c8ebba0b0ea1ffced5f093077605e7ff476
Thibault BRARD <brard.thibault0@gmail.com>
2024-04-16 09:00:30 +0200

    Installation ok sous windows
    - Procédure d'installation avec suivi.txt
    - deploy.py pour télécharger le modèle falcon7b

---
+++ install/windows/deploy.py
@@ -0,0 +1,17 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedModel
+import transformers
+import torch
+
+#Enter your local directory you want to store the model in
+save_path = "models"
+
+#Specify the model you want to download from HF
+hf_model = 'tiiuae/falcon-7b'
+
+#Instantiate the model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(hf_model, return_dict=True, trust_remote_code=True, torch_dtype=torch.bfloat16, device_map="auto",) # gpu torch_dtype=torch.bfloat16, device_map="auto",
+tokenizer = AutoTokenizer.from_pretrained(hf_model)
+
+#Save the model and the tokenizer in the local directory specified earlier
+model.save_pretrained(save_path)
+tokenizer.save_pretrained(save_path)
