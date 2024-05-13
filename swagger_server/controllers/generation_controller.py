3e1b2f4a669acfb0067882b8f846b9e12b0b9ddc
BRARD THIBAULT <thibault.brard@etu.unistra.fr>
2024-05-13 09:30:41 +0200

    Génération txt de sortie + gestion retour output
    - Les IA retournent désormais toutes des fichiers de sorties
    - La contrainte imposée est que chaque IA doit avoir une utilitée de
      type input2output

--- swagger_server/controllers/generation_controller.py
+++ swagger_server/controllers/generation_controller.py
@@ -3,8 +3,8 @@ import six
 import json
 import subprocess
 
-from os import listdir
-from os.path import isfile, join
+from os import listdir, mkdir
+from os.path import isfile, join, exists
 from swagger_server.models.img import Img  # noqa: E501
 from swagger_server import util
 
@@ -72,7 +72,26 @@ def generation_par_ia(nom_categorie, nom_ia, modele_ia:str, nom_fichier_parametr
     print("Sortie standard :", stdout.decode())
     print("Erreur standard :", stderr.decode())
 
-    return stdout.decode()
+    chemin = f'outputs/{nom_categorie}/{nom_ia}'
+    fichier = "00000.txt"
+    if(nom_categorie.split("2")[-1] == "txt"):
+        print("Write inside a txt file")
+        if(exists(chemin+'/'+fichier)):
+            fichier = listdir(chemin)[-1]
+            num_fichier = str(int(fichier[:-4])+1)
+            fichier = '0'*(5-len(num_fichier)) + num_fichier + ".txt"
+        else:
+            mkdir(f'outputs/{nom_categorie}')
+            mkdir(chemin)
+        with open(chemin+'/'+fichier, 'w') as f:
+            f.write(stdout.decode())
+    else:
+        fichier = listdir(chemin)[-1]
+        if(not isfile(chemin+'/'+fichier)):
+            fichier = listdir(chemin+'/'+fichier)[-1]
+
+    dictionnaire_retour = { "output": f'{chemin}/{fichier}'}
+    return dictionnaire_retour
 
 
 def get_resultat(nom_categorie, nom_ia):  # noqa: E501
