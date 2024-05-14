724d26e662afa8fda86a2133041209a4cb7dd5cb
BRARD THIBAULT <thibault.brard@etu.unistra.fr>
2024-05-14 16:14:43 +0200

    Création d'ia à partir d'un répertoire existant
    - Utilisation de symlink pour permettre de créer des liens symboliques

--- swagger_server/controllers/ia_controller.py
+++ swagger_server/controllers/ia_controller.py
@@ -1,12 +1,20 @@
 import connexion
 import six
 
-from os import listdir
+import subprocess
+from os import listdir, mkdir
 from os.path import isfile, exists
 from swagger_server.models.input_ia import InputIA  # noqa: E501
 from swagger_server.models.output import Output  # noqa: E501
 from swagger_server import util
 
+def send_symlink(src, dest):
+    process = subprocess.Popen(["./apiEnv/Scripts/python", "./toolkit/symlink.py", "-src", src, "-dest", dest], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode == 0:
+        print("Le subprocess s'est terminé avec succès.")
+    else:
+        print("Le subprocess a échoué avec le code de sortie :", process.returncode)
 
 def add_ia(body, nom_ia, categorie, utilisable):  # noqa: E501
     """Ajoute une nouvelle IA
@@ -24,9 +32,32 @@ def add_ia(body, nom_ia, categorie, utilisable):  # noqa: E501
 
     :rtype: Output
     """
-    if connexion.request.is_json:
-        body = InputIA.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    retour = {"output":""}
+    chemin_ia = f'./IA/{nom_ia}'
+    chemin_toolkit = f'./toolkit/{categorie}/{nom_ia}'
+    if(not exists(chemin_ia) and exists(f'./toolkit/{categorie}')):
+        mkdir(chemin_ia)
+        mkdir(chemin_toolkit)
+        chemin_install = chemin_ia+'/'+"install"
+        if(body["install_windows_chemin_absolu"] != ""):
+            mkdir(chemin_install)
+            send_symlink(body["install_windows_chemin_absolu"], chemin_install+'/'+"install_windows.ps1")
+        if(body["install_mac_chemin_absolu"]):
+            if(not exists(chemin_install)):
+                mkdir(chemin_install)
+            send_symlink(body["install_mac_chemin_absolu"], chemin_install+'/'+"install_mac.ps1")
+        if(body["install_linux_chemin_absolu"]):
+            if(not exists(chemin_install)):
+                mkdir(chemin_install)
+            send_symlink(body["install_linux_chemin_absolu"], chemin_install+'/'+"install_linux.ps1")
+        if(body["inference_chemin_absolu"]):
+            send_symlink(body["inference_chemin_absolu"], chemin_toolkit+'/'+"inference.py")
+        if(body["param_chemin_absolu"]):
+            send_symlink(body["param_chemin_absolu"], chemin_toolkit+'/'+"param.json")
+        if(body["venv_chemin_absolu"]):
+            send_symlink(body["venv_chemin_absolu"], chemin_ia+'/'+nom_ia+"Env")
+        retour["output"] = chemin_ia
+    return retour
 
 
 def get_ia(categorie):  # noqa: E501
